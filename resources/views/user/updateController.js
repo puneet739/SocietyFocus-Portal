868d9ba1b89fb88490bc76e208f6339e14c6f29f
puneetbehl <puneetb@valyoo.in>
2016-02-19 15:53:52 +0530

    Puneetb:: Beautification changes

--- resources/views/user/updateController.js
+++ resources/views/user/updateController.js
@@ -1,17 +1,26 @@
 app.controller("updateController", function($scope,$rootScope,$http) {
 	console.log($rootScope.logedinuser);
-	$scope.updateUser = function(logedinuser) {
+
+	$scope.getCurrentUser= function(){
+		var req = {
+	            method: 'GET',
+	            url: $rootScope.constant.SERVICE_URL + '/access/currentuser',
+	        }
+	        $http(req).then(function successCallback(response) {
+	            $scope.currentUser = response.data.principal.user;
+	        });
+	}
+	$scope.updateUser = function() {
 	        var userString = JSON.stringify($rootScope.logedinuser, null, "\t");
 	        var req = {
 	            method: 'POST',
 	            url: $rootScope.constant.SERVICE_URL + '/user/modifyuser',
-	            data: userString,
+	            data:  $scope.currentUser,
 	        }
 	        $http(req).then(function successCallback(response) {
 	            console.log('user modified successfully');
 	            var allUsers = response.data.body;
-	            alert('User modified successfully');
+	            alert('User modified successfully, Kindly LogOut and Login to see new details');
 	        });
-	        console.log('Its time to save the user' + userString);
 	    };
 });
