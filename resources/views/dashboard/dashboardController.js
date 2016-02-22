8157f3def2cdb5f84dfe6cf11ecd3d046b218141
puneetbehl <puneetb@valyoo.in>
2016-02-22 16:34:29 +0530

    Puneetb:: Storing Society and displaying from localStorage

--- resources/views/dashboard/dashboardController.js
+++ resources/views/dashboard/dashboardController.js
@@ -1,7 +1,7 @@
-app.controller("dashboardController", function($scope, $sessionStorage, $http,$localStorage, $filter, $q, $location, $stateParams, $rootScope, toaster) {
+app.controller("dashboardController", function($scope, $localStorage, $http,$localStorage, $filter, $q, $location, $stateParams, $rootScope, toaster) {
 
 	$scope.init = function(){
-		$scope.currentSociety = $sessionStorage.society;
+		$scope.currentSociety = $localStorage.society;
 	}
 
 });
