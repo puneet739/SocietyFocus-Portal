161f0b28eeb470220617760f0b0632d3db93f8bf
puneetbehl <puneetb@valyoo.in>
2016-03-02 00:02:45 +0530

    Puneetb:: Tracking Angularatics

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,6 +2,7 @@ app.run(['$rootScope', '$state', '$stateParams', '$window', 'authentication', '$
     function($rootScope, $state, $stateParams, $window, authentication, $cookieStore, Analytics) {
 
         $rootScope.constant={
+            // SERVICE_URL:"http://zircon.com/localservice"
             SERVICE_URL:"http://societyfocus.com/service"
         }
         // Attach Fastclick for eliminating the 300ms delay between a physical tap and the firing of a click event on mobile browsers
@@ -145,6 +146,8 @@ app.config(['AnalyticsProvider',
            { tracker: 'UA-74141978-1', name: "tracker1" }
         ]);
         AnalyticsProvider.trackPages(true);
+        AnalyticsProvider.trackUrlParams(true);
+
     }
 ]);
 
