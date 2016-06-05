e3afdfa7e93ed66cb5b25a837a0e1658e531566d
puneetbehl <puneetb@valyoo.in>
2016-06-05 15:54:32 +0530

    Puneetb:: Helpdesk page adding advertisements

--- helpdesk/js/helpdeskcontroller.js
+++ helpdesk/js/helpdeskcontroller.js
@@ -23,17 +23,28 @@ app.config(function ($routeProvider,$locationProvider){
 
 app.filter('timeago',function(){
   return function(input) {
+    if (input==null) return null;
     var date=input.replace("T"," ");
     var timeAgo = moment(new Date(date)).fromNow()
     return timeAgo;
   };
 });
+app.directive('ads', function() {
+    return {
+        restrict: 'A',
+        templateUrl: 'views/Adsensecode.html',
+        controller: function(){
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        }
+    };
+});
+
 
 app.run(function($rootScope) {
     $rootScope.constant={
-            // SERVICE_URL:"http://zircon.com/zservice",
+            SERVICE_URL:"http://zircon.com/zservice",
             APP_PREFIX:"http://societyfocus.com/helpdesk",
-            SERVICE_URL:"http://societyfocus.com/service",
+            // SERVICE_URL:"http://societyfocus.com/service",
             name: 'Society Focus Helpdesk, Forum for all society related issues, maintaince, Legal Advice, Resource handling, Parking issues.',
             description: 'Society Focus Helpdesk, Forum for all society related issues, maintaince, Legal Advice, Resource handling, Parking issues.',
         }
@@ -53,6 +64,14 @@ app.controller("helpdeskController", function($scope,$http,$rootScope) {
            $scope.totalCount = totalCount % pageSize!=0 ? Math.ceil(totalCount/pageSize) : totalCount/pageSize;
         });
     }
+
+    $scope.showAds = function(unitCount){
+        console.log("Current unit count is"+unitCount);
+        var result=unitCount%5;
+        if (result==0) return true
+        return false;
+    }
+
  });
 app.controller("questionController", function($scope,$http,$rootScope,$routeParams,$anchorScroll,$location) {
     $scope.scrollTo = function(id) {
