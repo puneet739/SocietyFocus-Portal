2f90fb1793e1523f8f06b849dbfabe68de3dcfd7
Puneet Behl <Puneet.Behl@hcentive.com>
2016-07-17 10:08:55 +0530

    Puneetb:: Now changing the default URL to localhost:8080

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,7 +2,7 @@ app.run(['$rootScope', '$state', '$stateParams', '$window', 'authentication', '$
     function($rootScope, $state, $stateParams, $window, authentication, $cookieStore, Analytics) {
 
         $rootScope.constant={
-            // SERVICE_URL:"http://zircon.com/localservice"
+            // SERVICE_URL:"http://localhost:8080/zircon/services"
             SERVICE_URL:"http://societyfocus.com/service"
         }
         // Attach Fastclick for eliminating the 300ms delay between a physical tap and the firing of a click event on mobile browsers
